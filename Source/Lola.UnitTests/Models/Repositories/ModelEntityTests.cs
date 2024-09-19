03d93949ab4181fecccba831700e03792cfa76fa
Andre Vianna <andre.vianna.rj@hotmail.com>
2024-09-19 00:53:43 -0400

    correct failing tests

--- Source/Lola.UnitTests/Models/Repositories/ModelEntityTests.cs
+++ Source/Lola.UnitTests/Models/Repositories/ModelEntityTests.cs
@@ -55,14 +55,14 @@ public class ModelEntityTests {
     public void ValidateKey_WithExistingKey_ShouldReturnError() {
         // Arrange
         const string key = "existingKey";
-        _mockModelHandler.GetById(Arg.Any<uint>()).Returns(new ModelEntity { Key = key });
+        _mockModelHandler.GetByKey(Arg.Any<string>()).Returns(new ModelEntity { Key = key });
 
         // Act
         var result = ModelEntity.ValidateKey(key, _mockModelHandler);
 
         // Assert
         result.IsInvalid.Should().BeTrue();
-        result.Errors.Should().Contain(e => e.Message == "A model with this identifier is already registered.");
+        result.Errors.Should().Contain(e => e.Message == "A model with this key is already registered.");
     }
 
     [Theory]
